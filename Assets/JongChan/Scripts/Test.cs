3e3f9354efc789ef6f09fc4204d3a39c05638313
blondlonghair <69098142+blondlonghair@users.noreply.github.com>
2020-12-13 02:46:58 +0900

    몬스터 움직임

---
+++ Assets/JongChan/Scripts/Test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    void Awake()
+    {
+        gameObject.GetComponent<Pathfinding.AIDestinationSetter>().enabled = true;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            gameObject.GetComponent<Pathfinding.AIDestinationSetter>().enabled = false;
+        }
+    }
+}
---
+++ Assets/heesuk/Scripts/UI/TraceOn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TraceOn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.parent.GetComponent<Pathfinding.AIDestinationSetter>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            this.transform.parent.GetComponent<Pathfinding.AIDestinationSetter>().enabled = true;
+        }
+
+        else
+        {
+            this.transform.parent.GetComponent<Pathfinding.AIDestinationSetter>().enabled = false;
+        }
+    }
+}
