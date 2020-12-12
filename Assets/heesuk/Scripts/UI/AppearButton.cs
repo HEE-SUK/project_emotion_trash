d1e961270367f5cecb00ccd0b4c589319ae4d0d5
henry <dltjr5177@gmail.com>
2020-12-13 02:08:54 +0900

    인트로 버튼

---
+++ Assets/heesuk/Scripts/UI/AppearButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class AppearButton : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        this.transform.localScale = Vector3.zero;
+        this.transform.DOScaleY(1f, 0.2f);
+        this.transform.DOScaleX(1f, 0.2f).SetDelay(0.05f).SetEase(Ease.OutBack);
+    }
+}
---
+++ Assets/heesuk/Scripts/UI/IntroCanvas.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class IntroCanvas : MonoBehaviour
+{
+
+    public void OnStartButton()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+    public void OnExitButton()
+    {
+        Application.Quit();
+    }
+}
