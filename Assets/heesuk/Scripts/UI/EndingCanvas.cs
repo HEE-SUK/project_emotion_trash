5cd18cb17b16f56517add02957635a3bdbb284e7
henry <dltjr5177@gmail.com>
2020-12-13 11:12:19 +0900

    인트로씬 수정

---
+++ Assets/heesuk/Scripts/UI/EndingCanvas.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingCanvas : MonoBehaviour
+{
+    [SerializeField]
+    private ChoicePanel choicePanel = null;
+
+    void Start()
+    {
+        AudioManager.SetVolumeBgm(0.3f);
+        AudioManager.PlayBgm(BGM.MAIN);
+        EventManager.on(EVENT_TYPE.GO_MAIN, this.GoMainScene);
+        EventManager.on(EVENT_TYPE.START_CHOICE, this.ShowChoicePanel);
+    }
+
+    public void ShowChoicePanel(EVENT_TYPE EventType, Component Sender, object Param = null)
+    {
+        List<Choice> choices = (List<Choice>)Param;
+
+        this.choicePanel.Init(choices);
+    }
+    public void GoMainScene(EVENT_TYPE EventType, Component Sender, object Param = null)
+    {
+        GameManager.Instance.nextSceneName = "Intro";
+        EventManager.emit(EVENT_TYPE.CLOSE_UP, this);
+    }
+    private void OnDestroy() {
+        
+        EventManager.off(EVENT_TYPE.START_CHOICE, this.ShowChoicePanel);
+        EventManager.off(EVENT_TYPE.GO_MAIN, this.GoMainScene);
+    }
+}
--- Assets/heesuk/Scripts/UI/IntroGameCanvas.cs
+++ Assets/heesuk/Scripts/UI/IntroGameCanvas.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
+using DG.Tweening;
 public class IntroGameCanvas : MonoBehaviour
 {
+    [SerializeField]
+    private CanvasGroup fadePanel = null;
+
     [SerializeField]
     private ChoicePanel choicePanel = null;
 
     void Start()
     {
+        this.fadePanel.alpha = 1f;
+        this.StartCoroutine(this.Fade(false, 0f, () => { }));
         AudioManager.SetVolumeBgm(0.3f);
         AudioManager.PlayBgm(BGM.MAIN);
         EventManager.on(EVENT_TYPE.GO_MAIN, this.GoMainScene);
@@ -24,12 +30,36 @@ public class IntroGameCanvas : MonoBehaviour
     }
     public void GoMainScene(EVENT_TYPE EventType, Component Sender, object Param = null)
     {
-        GameManager.Instance.nextSceneName = "Intro";
-        EventManager.emit(EVENT_TYPE.CLOSE_UP, this);
+        this.StartCoroutine(this.Fade(true, 1f, () => {
+            SceneManager.LoadScene("Intro");
+        }));
     }
     private void OnDestroy() {
         
         EventManager.off(EVENT_TYPE.START_CHOICE, this.ShowChoicePanel);
         EventManager.off(EVENT_TYPE.GO_MAIN, this.GoMainScene);
+    }
+
+    private IEnumerator Fade(bool _isUp ,  float _value, CallbackEvent _callback)
+    {
+        float value = (_isUp)? 0.05f : -0.05f;
+
+        if(_isUp)
+        {
+            while (this.fadePanel.alpha < _value)
+            {
+                this.fadePanel.alpha += value;
+                yield return new WaitForSeconds(0.05f);
+            }
+        }
+        else
+        {
+            while (this.fadePanel.alpha > _value)
+            {
+                this.fadePanel.alpha += value;
+                yield return new WaitForSeconds(0.05f);
+            }
+        }
+        _callback();
     }
 }
